ab425794c492b303ab04b23f59d3be3b6d0750cd
Mustafaefe <m.efe_1905@hotmail.com>
2024-08-04 22:48:57 +0300

    Initial Commit

---
+++ FitCal/FitCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitCalApp.swift
+//  FitCal
+//
+//  Created by Mustafa Efe on 4.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct FitCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
