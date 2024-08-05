7ec414d0f404bb6da94882aec4275da97c2d75ac
Mustafaefe <m.efe_1905@hotmail.com>
2024-08-05 22:54:33 +0300

    Demo app icon was added in Assets and changed project files.

--- FitCal/FitCalApp.swift
+++ FitCal/FitCalApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct FitCalApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //ContentView()
+            AppInitialView()
         }
     }
 }
